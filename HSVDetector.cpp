9470d694c7b1560f64a5bd8386b5244cc6b8971a
Niklas Schalck Johansson <dyrborg2880@gmail.com>
2013-10-01 15:17:35 +0200

    Added contrast function. Updated HSVDetector to now properly detect red and green on 4.jpg.

--- HSVDetector.cpp
+++ HSVDetector.cpp
@@ -16,26 +16,59 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/features2d/features2d.hpp>
 #include "HSVDetector.h"
+#include "Contrast.h"
 
 using namespace cv;
 using namespace std;
 
 int HSVDetection(){
-    string filename = "/Users/Niklas/Desktop/redblue.png";
+    //File location
+    string filename = "/Users/Niklas/Developer/TermiteTracker/Media/4.jpg";
     
+    //Define matrices
     Mat img = imread(filename);
-    
-    Mat HSVimg;
+    Mat contrast_img = constrastImage(img);
     Mat imgThreshRed;
-    Mat imgThreshBlue;
+    Mat imgThreshGreen;
+    Mat blob;
     
+    //Threshold based on color ranges (Blue/Green/Red scalars)
+    inRange(contrast_img, Scalar(100,100,250), Scalar(200,200,255), imgThreshRed); //RGB range
+    inRange(contrast_img, Scalar(100,250,100), Scalar(200,255,200), imgThreshGreen); //RGB range
+
+    //Apply Blur effect to make blobs much more coherent
+    GaussianBlur(imgThreshRed, imgThreshRed, Size(3,3), 0);
+    GaussianBlur(imgThreshGreen, imgThreshGreen, Size(3,3), 0);
     
-    cvtColor(img, HSVimg, CV_BGR2HSV);
-    inRange(HSVimg, Scalar(0,100,51), Scalar(0,256,256), imgThreshRed);
-    inRange(HSVimg, Scalar(120,100,40), Scalar(120,256,256), imgThreshBlue);
+    //Set SimpleBlobDetector parameters
+    SimpleBlobDetector::Params params;
+    params.filterByArea = false;
+    params.filterByCircularity = false;
+    params.filterByConvexity = false;
+    params.filterByInertia = false;
+    params.filterByColor = true;
+    params.blobColor = 255;
     
-    imshow("Red Threshhold", imgThreshRed);
-    imshow("Blue Threshhold", imgThreshBlue);
+    SimpleBlobDetector blobDetector(params);
+    blobDetector.create("Blob Detection");
+    
+    //Vectors to store keypoints (center points for a blob)
+    vector<KeyPoint> redKeyPoints;
+    vector<KeyPoint> greenKeyPoints;
+    
+    //Try blob detection for both thresholded colors
+    blobDetector.detect(imgThreshRed, redKeyPoints);
+    blobDetector.detect(imgThreshGreen, greenKeyPoints);
+    
+    //Draw resulting keypoints
+    drawKeypoints(img, redKeyPoints, blob, CV_RGB(255,255,0), DrawMatchesFlags::DEFAULT);
+    drawKeypoints(blob, greenKeyPoints, blob, CV_RGB(249,0,255), DrawMatchesFlags::DEFAULT);
+    
+    //Show resulting images from program execution
+    imshow("Detect Red and Green", blob);
+    imshow("Green threshold", imgThreshGreen);
+    imshow("Red threshold", imgThreshRed);
+    imshow("Contrasted image", contrast_img);
     
     waitKey(0);
     
