1f1f253f276789ed747c8cf76069120385aa40ed
Nikolaj <nikolajaaes@gmail.com>
2013-10-22 14:31:40 +0200

    Resizing for 16:9 and 4:3 for the video feeds in the GUI

--- GUI/TERMESGUI.java
+++ GUI/TERMESGUI.java
@@ -12,6 +12,9 @@ public class TERMESGUI extends JFrame
 	
 	JLabel picLabel;
 	ImageIcon icon;
+	
+	int frameHeight;
+	int frameWidth;
 
 	public TERMESGUI()
 	{
@@ -21,8 +24,10 @@ public class TERMESGUI extends JFrame
 		JLabel label = new JLabel(TERMESConnector.test());
 		pane.add(label);
 		
+		//setup input feed
 		TERMESConnector.start();
 		
+		//initialize the icon and label that will contain the video
 		icon = new ImageIcon();
 		picLabel = new JLabel(icon);
 		add(picLabel);
@@ -30,18 +35,24 @@ public class TERMESGUI extends JFrame
 		setLayoutConstraints();
 		setVisible(true); // display this frame
 		
-		this.addWindowListener( new WindowAdapter() {
-            @Override
-            public void windowClosing(WindowEvent we) {
-               TERMESConnector.releaseCamera();
-               System.exit(0);
-            }
-        } );
-		
+		//poll for the next frame as long as there is one
 		byte[] frame;
 		while((frame = TERMESConnector.getNextFrame()) != null)
 		{
-			icon = new ImageIcon(TERMESImageProcessing.convertByteArrayToImage(frame));
+			//get the next frame as an image
+			Image img = TERMESImageProcessing.convertByteArrayToImage(frame); 
+			
+			//scale the image if necessary
+			if (frameHeight == 0) //then frameWidth is also 0
+			{
+				frameHeight = determineFrameHeight(img.getWidth(null), img.getHeight(null));
+				frameWidth = determineFrameWidth(img.getWidth(null), img.getHeight(null));
+			}
+			
+			img = img.getScaledInstance(frameWidth, frameHeight ,java.awt.Image.SCALE_SMOOTH );  
+			
+			icon = new ImageIcon(img);
+
 			picLabel.setIcon(icon);
 			
 			try {
@@ -52,26 +63,50 @@ public class TERMESGUI extends JFrame
 			}
 		}
 		
-		
 	}
 	
 	public void InitializeWindow()
 	{
 		setTitle("TERMES");
 		setBounds(100, 100, 1100, 700);
-		//setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		Container con = this.getContentPane(); 
 		con.add(pane);
 		
 		layout = new SpringLayout();
 		setLayout(layout);
+		
+		//see to that the camera is released when the window is closed 
+		// (if this isn't done, the application will crash when it is closed)
+		this.addWindowListener( new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent we) {
+               TERMESConnector.releaseCamera();
+               System.exit(0);
+            }
+        } );
 	}
 	
 	public void setLayoutConstraints()
 	{
 		layout.putConstraint(SpringLayout.WEST, picLabel, 50 ,SpringLayout.WEST, pane);
 		layout.putConstraint(SpringLayout.NORTH, picLabel, 50 ,SpringLayout.NORTH, pane);
+	}
+	
+	public int determineFrameHeight(double width, double height)
+	{	
+		if(width/height == 16.0/9.0) // 16:9
+			return 360;
+		else // 4:3
+			return 480;
+	}
+	
+	public int determineFrameWidth(double width, double height)
+	{
+		if(width/height == 16.0/9.0) // 16:9
+			return 640;
+		else // 4:3
+			return 640;
 	}
 	
 }
